8d259d32e378d78d50530e7a17b949a2af10ffc9
EkaThe17th <58234455+EkaThe17th@users.noreply.github.com>
2023-12-09 19:29:07 +0200

    Add files via upload

---
+++ pythonCRC.py
@@ -0,0 +1,7 @@
+import datetime
+
+currentTime = datetime.datetime.now()
+
+print("Hello, this is a simple .py file made by Mohamed El-Moshrefy to allow for testing his Redhat OpenShift Adventures")
+
+print("The current time in Egypt, Cairo is:", currentTime)
